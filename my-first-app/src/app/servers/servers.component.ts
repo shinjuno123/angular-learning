a403367fcf45542f063947e6d0df095660366081
Junho Shin <shinjuno123@naver.com>
2023-04-28 13:06:30 -0700

    Learn : Learn angular basics

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector:'.app-servers',
+  selector: 'app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
+  templateUrl : './servers.component.html',
+ styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+  serverName = '';
+
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
