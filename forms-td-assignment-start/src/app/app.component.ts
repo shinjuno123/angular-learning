081a1cc7df4a0ce5c95a5fb87e1ca768c99dc800
Junho Shin <shinjuno123@naver.com>
2023-05-03 15:39:18 -0700

    Learn : Finish form TD assignment

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("formData") formData: NgForm;
+  defaultSubscription = 'advanced';
+  submitData = {email: '', subscriptions: '', passsword:''}
+  submitted = false;
+  subscriptions = ['basic', 'advanced', 'pro']
 
+  onSubmit(){
+    this.submitted = true;
+    this.submitData = this.formData.value;
+    this.formData.reset();
+    this.formData.form.patchValue({
+      subscriptions: 'advanced'
+    })
+  }
 }
