a56e7a42a24943b9c3bd3b523256960576bae095
Junho Shin <shinjuno123@naver.com>
2023-05-04 20:22:53 -0700

    Apply http to shopping recipe application

---
+++ http-01-start/src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Post } from './post.model';
+import { PostService } from './posts.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private errorSub: Subscription;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.errorSub = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
+
+    this.isFetching = true;
+    this.postService.fetchPost().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error =>{
+      this.isFetching = false;
+      this.error = error.message;
+    });
+  }
+
+  onCreatePost(postData: Post,) {
+    this.postService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.isFetching = true;
+    this.postService.fetchPost().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error =>{
+      this.isFetching = false;
+      this.error = error.message;
+    });
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postService.deletePosts().subscribe(
+      () => {
+        this.loadedPosts = [];
+      }
+    )
+  }
+
+  onHandleError(){
+    this.error = null;
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
+}
--- shopping-recipe-app/src/app/recipes/recipe.service.ts
+++ shopping-recipe-app/src/app/recipes/recipe.service.ts
@@ -8,48 +8,55 @@ import { Subject } from "rxjs";
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Panang Curry',
-      'Quick and Easy Panang Curry',
-      'https://tastesbetterfromscratch.com/wp-content/uploads/2019/12/Panang-Curry-6.jpg',
-      [
-        new Ingredient("tablespoon oil", 1),
-        new Ingredient("tablespoons Panang curry paste", 2),
-        new Ingredient("tablespoon peanut butter", 1),
-        new Ingredient("pounds chicken breasts , cut into pieces against the grain", 2),
-        new Ingredient("small onion , sliced", 1),
-        new Ingredient("green bell pepper , thinly sliced", 1),
-        new Ingredient("red bell pepper , thinly sliced", 1),
-        new Ingredient("teaspoons freshly grated ginger", 2),
-        new Ingredient("garlic cloves , minced", 4),
-        new Ingredient("14 oz cans coconut milk (Chaokoh brand is my favorite)", 2),
-        new Ingredient("teaspoons cornstarch", 2),
-        new Ingredient("tablespoons fish sauce", 1),
-        new Ingredient("tablespoon lime juice", 1),
-        new Ingredient("loosely packed basil leaves , roughly chopped", 1),
-        new Ingredient("▢Hot cooked rice white, brown or jasmine", 1)
-      ]),
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Panang Curry',
+  //     'Quick and Easy Panang Curry',
+  //     'https://tastesbetterfromscratch.com/wp-content/uploads/2019/12/Panang-Curry-6.jpg',
+  //     [
+  //       new Ingredient("tablespoon oil", 1),
+  //       new Ingredient("tablespoons Panang curry paste", 2),
+  //       new Ingredient("tablespoon peanut butter", 1),
+  //       new Ingredient("pounds chicken breasts , cut into pieces against the grain", 2),
+  //       new Ingredient("small onion , sliced", 1),
+  //       new Ingredient("green bell pepper , thinly sliced", 1),
+  //       new Ingredient("red bell pepper , thinly sliced", 1),
+  //       new Ingredient("teaspoons freshly grated ginger", 2),
+  //       new Ingredient("garlic cloves , minced", 4),
+  //       new Ingredient("14 oz cans coconut milk (Chaokoh brand is my favorite)", 2),
+  //       new Ingredient("teaspoons cornstarch", 2),
+  //       new Ingredient("tablespoons fish sauce", 1),
+  //       new Ingredient("tablespoon lime juice", 1),
+  //       new Ingredient("loosely packed basil leaves , roughly chopped", 1),
+  //       new Ingredient("▢Hot cooked rice white, brown or jasmine", 1)
+  //     ]),
 
-    new Recipe(
-      'CHICKEN FAJITAS',
-      'Easy and flavorful weeknight meal',
-      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/02/Chicken-Fajitas-7-1024x1536.jpg',
-      [new Ingredient("Chili Powder", 1),
-      new Ingredient("Ground Cumin", 1),
-      new Ingredient("Garlic Powder", 1),
-      new Ingredient("Paprika", 1),
-      new Ingredient("Oregano", 1),
-      new Ingredient("Salt", 1),
-      new Ingredient("Pepper", 1)
-      ])
-  ];
+  //   new Recipe(
+  //     'CHICKEN FAJITAS',
+  //     'Easy and flavorful weeknight meal',
+  //     'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2020/02/Chicken-Fajitas-7-1024x1536.jpg',
+  //     [new Ingredient("Chili Powder", 1),
+  //     new Ingredient("Ground Cumin", 1),
+  //     new Ingredient("Garlic Powder", 1),
+  //     new Ingredient("Paprika", 1),
+  //     new Ingredient("Oregano", 1),
+  //     new Ingredient("Salt", 1),
+  //     new Ingredient("Pepper", 1)
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) { }
 
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
   }
 
   getRecipe(id: number) {
